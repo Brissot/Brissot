231eb48e9cd87b7acfb33a550806ebc23cd1e3c8
Brissot <sidsu7@gmail.com>
2025-07-07 23:41:20 -0400

    Firefox Fix
    A small fix to make the stoplight buttons correctly aligned in Firefox

--- dotfiles/apply_dotfiles.sh
+++ dotfiles/apply_dotfiles.sh
@@ -4,3 +4,5 @@ cp -i emacs.config.el ~/.config/emacs/init.el
 
 ## if we want this
 # cp -i emacsclient.desktop ~/.local/share/applications/emacsclient.desktop
+
+cp -i firefox_customization.css ~/.mozilla/firefox/firefox-themes/customChrome.css
